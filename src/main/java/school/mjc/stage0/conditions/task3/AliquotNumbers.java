c3d141337da044d5befb6868dfc8a78451a915d1
jargr <jaroslav.grablevski@gmail.com>
2023-02-21 15:01:03 +0200

    Add if's

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,5 +2,6 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
+        System.out.println(second % first == 0?"Aliquot":"Not aliquot");
     }
 }
